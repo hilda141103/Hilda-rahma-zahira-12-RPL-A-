3510575b334ae68707736c362f0b84151b9d83ca
hilda141103 <70936547+hilda141103@users.noreply.github.com>
2020-09-08 14:52:37 +0700

    Add files via upload

---
+++ src/uts2020/Uts2020.java
@@ -0,0 +1,25 @@
+
+package uts2020;
+
+
+public class Uts2020 {
+
+    
+    public static void main(String[] args) {
+            String myString1 = "H,I,L,D,A-R,A,H,M,A";
+
+        System.out.println(myString1.charAt(0));
+        System.out.println(myString1.charAt(2));
+        System.out.println(myString1.charAt(4));
+        System.out.println(myString1.charAt(6));
+        System.out.println(myString1.charAt(8));
+        System.out.println(myString1.charAt(9));
+        System.out.println(myString1.charAt(10));
+        System.out.println(myString1.charAt(12));
+        System.out.println(myString1.charAt(14));
+        System.out.println(myString1.charAt(16));
+        System.out.println(myString1.charAt(18));
+
+    }
+    
+}
